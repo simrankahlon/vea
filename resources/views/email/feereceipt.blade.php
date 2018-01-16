bd253463444b3b7c9ac06fe4487ee9add9dbba07
simrankahlon <simran.kahlon@atos.net>
2018-01-16 20:24:41 +0530

    Email value
    Signed-off-by: simrankahlon <simran.kahlon@atos.net>

--- resources/views/email/feereceipt.blade.php
+++ resources/views/email/feereceipt.blade.php
@@ -6,6 +6,8 @@
             HELLO,
             <br><br>
             PFA-The Installment Receipt.
+            <br><br>
+            This is an auto generated mail,please do not reply .
             </div>
         </div><!--END DIV ROW-->
     </div><!--END CARD-BLOCK-->
